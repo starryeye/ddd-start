bfd14c6e4342361998137eeff2f8088953454386
starryeye <see2818@naver.com>
2024-05-15 20:50:12 +0900

    [starryeye, 2024.05.15] - domain model pattern
    내용
    - domain model pattern

--- shop/src/main/java/dev/practice/shop/ShopApplication.java
+++ shop/src/main/java/dev/practice/shop/ShopApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopApplication {
 
+	/**
+	 * Domain Driven Design
+	 *
+	 * [BIZ] -> 핵심 비즈니스 규칙
+	 *
+	 * index..
+	 * 1. 도메인, 도메인 모델 패턴, 도메인 모델 -> Order
+	 *
+	 *
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
